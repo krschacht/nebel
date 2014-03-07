9ae493d5cbd451dcd54d3ac6de3306c4a3c3982b
Avand <avand@avandamiri.com>
2014-03-07 12:09:17 -0800

    Adding presence validation to topic_id of Exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,5 @@
 class Exercise < ActiveRecord::Base
-  validates_presence_of :name
+  validates_presence_of :name, :topic_id
 
   belongs_to :topic
 end
--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -7,6 +7,12 @@ class ExerciseTest < ActiveSupport::TestCase
     assert exercise.errors[:name].include? "can't be blank"
   end
 
+  test "validates presence of topic_id" do
+    exercise = Exercise.new
+    assert !exercise.valid?
+    assert exercise.errors[:topic_id].include? "can't be blank"
+  end
+
   test "belongs to topic" do
     assert_equal topics(:d5), exercises(:d5_part1).topic
   end
