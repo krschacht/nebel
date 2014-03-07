ca82ee6b0a7325656ce1d95187c0754e4e2e416b
Avand <avand@avandamiri.com>
2014-03-07 12:34:50 -0800

    Refactor topics views
    I created a topics layout that we can share for the exercises views
    shortly. I also broke out a route for each field of a lesson, since
    they are each quite beefy. Each field has its own partial, which lets
    us customize the view for each field.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Bfsu::Application.routes.draw do
 
   root "topics#index"
 
+  get "/topics/:id(/:field)", to: "topics#show", as: "topic", defaults: { field: "overview" }
+
   resources :topics
   resources :exercises
 
