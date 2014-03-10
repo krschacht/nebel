02283ad3baa8e93912cf1fa2c6ffcb0288f2cfbf
Avand <avand@avandamiri.com>
2014-03-10 11:30:33 -0700

    Requisition validates presence of exercise_id and material_id

--- app/models/requisition.rb
+++ app/models/requisition.rb
@@ -1,2 +1,3 @@
 class Requisition < ActiveRecord::Base
+  validates_presence_of :exercise_id, :material_id
 end
--- test/models/requisition_test.rb
+++ test/models/requisition_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
 class RequisitionTest < ActiveSupport::TestCase
+  test "validates presence of exercise_id" do
+    requisition = Requisition.new
+    assert !requisition.valid?
+    assert requisition.errors[:exercise_id].include? "can't be blank"
+  end
+
+  test "validates presence of material_id" do
+    requisition = Requisition.new
+    assert !requisition.valid?
+    assert requisition.errors[:material_id].include? "can't be blank"
+  end
+
   test "quantity defaults to 1" do
     assert_equal 1, Requisition.new.quantity
   end
