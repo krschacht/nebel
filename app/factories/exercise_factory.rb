a5e74dfdc18b9548eea1451d1db7620715d9804e
Avand <avand@avandamiri.com>
2014-03-10 13:45:43 -0700

    ExerciseFactory now uses LessonPartSplitter

--- app/factories/exercise_factory.rb
+++ app/factories/exercise_factory.rb
@@ -1,47 +1,25 @@
 require "book"
+require "lesson_part_splitter"
 
 class ExerciseFactory
 
   def initialize(book_lesson)
     @book_lesson = book_lesson
+    @lesson_part_splitter = LessonPartSplitter.new(@book_lesson.methods_and_procedures)
   end
 
   def exercises
-    part_names = @book_lesson.methods_and_procedures.scan(/^Part\s\d{1,2}.*$/)
-    part_names.map.with_index do |part_name, i|
-      clean_part_name = part_name.strip.gsub(/\.$/, "")
-      part_index      = i + 1
-      body            = text_between_parts(@book_lesson.methods_and_procedures, part_index)
-      materials       = text_between_parts(@book_lesson.materials, part_index)
-      materials     ||= @book_lesson.materials
-      duration        = @book_lesson.time_required[/^Part #{i + 1}.*\(\s*(.*)\s*\)/, 1]
-      duration      ||= @book_lesson.time_required
+    @lesson_part_splitter.split do |part_number, part_name, part_text|
+      duration   = @book_lesson.time_required[/^Part #{part_number}.*\(\s*(.*)\s*\)/, 1]
+      duration ||= @book_lesson.time_required
 
       Exercise.new({
-        name:      clean_part_name,
-        body:      body,
+        name:      part_name,
+        body:      part_text,
         duration:  duration,
-        materials: materials,
-        order:     order_from_name(clean_part_name)
+        order:     part_number
       })
     end
-  end
-
-private
-
-  def text_between_parts(text, part_index)
-    # http://rubular.com/r/vm39sWyUyY
-    not_last_part_pattern = /Part\s#{part_index}\.[^\n]*(.*)\nPart\s#{part_index + 1}\./m
-    last_part_pattern     = /Part\s#{part_index}\.[^\n]*(.*)/m
-
-    match   = text[not_last_part_pattern, 1]
-    match ||= text[last_part_pattern, 1]
-
-    match.try :clean
-  end
-
-  def order_from_name(name)
-    name[/^Part\s(\d)\./, 1].to_i
   end
 
 end
