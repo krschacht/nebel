b63dd46f8bcaecdbb40b67bc4f134efb1ff833d0
Avand <avand@avandamiri.com>
2014-03-10 11:06:19 -0700

    New migration: create materials

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308024956) do
+ActiveRecord::Schema.define(version: 20140310175411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20140308024956) do
   end
 
   add_index "exercises", ["topic_id"], name: "index_exercises_on_topic_id", using: :btree
+
+  create_table "materials", force: true do |t|
+    t.string   "name"
+    t.text     "original_name"
+    t.text     "description"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "prerequisites", force: true do |t|
     t.integer "topic_id"
