b97d29fef80b988229e1c848170617af3460a6be
Avand <avand@avandamiri.com>
2014-03-21 15:24:32 -0700

    New migration: create users

--- db/schema.rb
+++ db/schema.rb
@@ -82,4 +82,13 @@ ActiveRecord::Schema.define(version: 20140322023430) do
   add_index "topics", ["slug"], name: "index_topics_on_slug", unique: true, using: :btree
   add_index "topics", ["subject_id"], name: "index_topics_on_subject_id", using: :btree
 
+  create_table "users", force: true do |t|
+    t.string   "name",          null: false
+    t.string   "email",         null: false
+    t.string   "password_hash", null: false
+    t.string   "access_token",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
