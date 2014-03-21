365e03c439ba16dded252dc9b5f543fe4e1851ba
Avand <avand@avandamiri.com>
2014-03-21 16:59:12 -0700

    Scaffold user resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Bfsu::Application.routes.draw do
 
   delete "/requisitions", to: "requisitions#destroy", as: "requisition"
 
+  resources :users
   resources :topics,    except: :show
   resources :exercises, except: [:index, :show]
   resources :materials, except: :show do
@@ -12,6 +13,5 @@ Bfsu::Application.routes.draw do
 
   get "/topics/:topic_slug/part-:part", to: "exercises#show", as: "canonical_exercise"
   get "/topics/:slug(/:field)", to: "topics#show", as: "canonical_topic", defaults: { field: "overview" }
-
 
 end
