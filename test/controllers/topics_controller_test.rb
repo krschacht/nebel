0015bfe2a6bd26b946dab2ebe9de26c40ea1cc3a
Avand <avand@avandamiri.com>
2014-03-25 11:55:58 -0700

    ApplicationController gets require_user and require_admin
    We now have two methods of restricting access to a controller action:
    require_user and require_admin. Both these methods rely on a user_id
    to be in session. If the check fails, they redirect to the homepage
    with an error message.
    
    I implemented these checks on TopicsController and reworked the tests
    for that controller to reflect the new behavior.

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,26 +1,57 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionController::TestCase
+
   setup do
     @topic = topics(:a2)
   end
 
-  test "should get index" do
+  test "GET to index requires a current user" do
     get :index
+
+    assert_user_required
+  end
+
+  test "GET to index loads subjects and topics by subject and renders index" do
+    login_as_user
+
+    get :index
+
     assert_response :success
+    assert_template :index
     assert_not_nil assigns(:subjects)
     assert_not_nil assigns(:topics_by_subject)
   end
 
-  test "should get new" do
+  test "GET to new requires an admin" do
+    login_as_user
+
     get :new
+
+    assert_admin_required
+  end
+
+  test "GET to new renders new" do
+    login_as_admin
+
+    get :new
+
     assert_response :success
   end
 
-  test "should create topic" do
+  test "POST to create requires an admin" do
+    login_as_user
+
+    post :create
+
+    assert_admin_required
+  end
+
+  test "POST to create creates a topic and redirects to it" do
+    login_as_admin
     @topic.destroy
 
-    assert_difference('Topic.count') do
+    assert_difference("Topic.count") do
       post :create, topic: {
         subject_id: @topic.subject_id, code: @topic.code, name: @topic.name,
         order: @topic.order
@@ -28,28 +59,75 @@ class TopicsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to canonical_topic_path(assigns(:topic))
+    assert_equal flash[:notice], "Topic was successfully created."
   end
 
-  test "should show topic" do
+  test "GET to show loads topic and renders show" do
+    login_as_user
+
     get :show, slug: @topic.slug, field: "overview"
+
     assert_response :success
+    assert_equal @topic, assigns(:topic)
+    assert_template :show
   end
 
-  test "should get edit" do
+  test "GET to edit requires an admin" do
+    login_as_user
+
     get :edit, id: @topic
+
+    assert_admin_required
+  end
+
+  test "GET to edit loads topic and renders edit" do
+    login_as_admin
+
+    get :edit, id: @topic
+
     assert_response :success
+    assert_template :edit
+    assert_equal @topic, assigns(:topic)
   end
 
-  test "should update topic" do
-    patch :update, id: @topic, topic: { code: @topic.code, name: @topic.name, overview: @topic.overview }
+  test "PATCH to update requires an admin" do
+    login_as_user
+
+    patch :update, id: @topic
+
+    assert_admin_required
+  end
+
+  test "PATCH to update updates the topic and redirects to it" do
+    login_as_admin
+
+    patch :update, id: @topic, topic: {
+      code: @topic.code, name: @topic.name, overview: @topic.overview
+    }
+
     assert_redirected_to canonical_topic_path(assigns(:topic))
+    assert_equal flash[:notice], "Topic was successfully updated."
+    assert_equal @topic, assigns(:topic)
   end
 
-  test "should destroy topic" do
-    assert_difference('Topic.count', -1) do
+  test "DELETE to destroy requires an admin" do
+    login_as_user
+
+    delete :destroy, id: @topic
+
+    assert_admin_required
+  end
+
+  test "DELETE to destroy destroys topic" do
+    login_as_admin
+
+    assert_difference("Topic.count", -1) do
       delete :destroy, id: @topic
     end
 
     assert_redirected_to topics_path
+    assert_equal flash[:notice], "Topic was successfully destroyed."
+    assert_equal @topic, assigns(:topic)
   end
+
 end
