9e66ffedb304fea90019a68bdaa7d29b56703124
Avand <avand@avandamiri.com>
2014-04-04 16:54:05 -0700

    Replacing new lines in StringExtensions correctly

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -1,6 +1,6 @@
 module StringExtensions
   def clean
     return nil if self.blank?
-    self.gsub(/\u2028/, "\n").gsub(/(\A(\n|\s)*|(\n|\s)*\z)/, "")
+    self.gsub(/\u2028/, "\n").gsub(/(\A(\\n|\s)*|(\\n|\s)*\z)/, "")
   end
 end
