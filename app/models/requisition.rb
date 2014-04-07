d56dd2174da1daa8f2557f32f40086ee052a677d
Keith Schacht <krschacht@gmail.com>
2014-04-07 14:48:45 -0700

    Allow for blank quantity count

--- app/models/requisition.rb
+++ app/models/requisition.rb
@@ -1,6 +1,5 @@
 class Requisition < ActiveRecord::Base
   validates_presence_of :exercise_id, :material_id
-  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 
   belongs_to :exercise
   belongs_to :material
