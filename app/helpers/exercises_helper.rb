2d9272bb2084cdd0f22b5d242f5b2dc653d214a3
Keith Schacht <krschacht@gmail.com>
2014-04-11 10:28:31 -0700

    Fix bug with nil material name

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,8 +1,9 @@
 module ExercisesHelper
 
   def pluralize_ending_for( str )
+    return if str.nil?
     last_word = str.split(' ').last
-    if last_word.pluralize.length > last_word.length
+    if last_word.to_s.pluralize.length > last_word.to_s.length
       last_word.pluralize[(( last_word.pluralize.length - last_word.length )*-1)..-1]
     end
   end
