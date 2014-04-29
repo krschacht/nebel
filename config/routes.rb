b0617a33810e410bd7fffe38fe28a95eae478080
Avand <avand@avandamiri.com>
2014-04-29 12:42:54 -0700

    Closes #55: Hand-writing routes to much clearer

--- config/routes.rb
+++ config/routes.rb
@@ -2,30 +2,55 @@ Nebel::Application.routes.draw do
 
   root "welcome#index"
 
-  resources :subscriptions, only: [:new, :create] do
-    patch :update, on: :collection
-    delete :destroy, on: :collection
-  end
-  resources :requisitions, only: [:create, :update, :destroy]
-  resources :users, only: [:new, :edit, :update, :create] do
-    get :forgot_password, on: :collection
-    post :send_access_email, on: :collection
-  end
-  resources :topics,    except: :show
-  resources :exercises, except: [:index, :show]
-  resources :messages, only: [:show, :index, :create, :destroy] do
-    patch :toggle, on: :member
-  end
-  resources :sessions, only: [:new, :create] do
-    delete :destroy, on: :collection
-  end
-  resources :materials, except: :show do
-    post :merge, on: :collection
-  end
+  get    "/topics"          => "topics#index",  as: "topics"
+  get    "/topics/new"      => "topics#new",    as: "new_topic"
+  get    "/topics/:id/edit" => "topics#edit",   as: "edit_topic"
+  get    "/topics/:slug"    => "topics#show",   as: "canonical_topic"
+  post   "/topics"          => "topics#create"
+  patch  "/topics/:id"      => "topics#update", as: "topic"
+  delete "/topics/:id"      => "topics#destroy"
+
+  get    "/exercises/new"      => "exercises#new",    as: "new_exercise"
+  get    "/exercises/:id/edit" => "exercises#edit",   as: "edit_exercise"
+  post   "/exercises"          => "exercises#create", as: "exercises"
+  patch  "/exercises/:id"      => "exercises#update", as: "exercise"
+  delete "/exercises/:id"      => "exercises#destroy"
+
+  get    "/materials"          => "materials#index",  as: "materials"
+  get    "/materials/new"      => "materials#new",    as: "new_material"
+  get    "/materials/:id/edit" => "materials#edit",   as: "edit_material"
+  post   "/materials"          => "materials#create"
+  post   "/materials/merge"    => "materials#merge",  as: "merge_materials"
+  patch  "/materials/:id"      => "materials#update", as: "material"
+  delete "/materials/:id"      => "materials#destroy"
+
+  post   "/requisitions"     => "requisitions#create", as: "requisitions"
+  patch  "/requisitions/:id" => "requisitions#update", as: "requisition"
+  delete "/requisitions/:id" => "requisitions#destroy"
+
+  get    "/messages"            => "messages#index",  as: "messages"
+  get    "/messages/:id"        => "messages#show",   as: "message"
+  post   "/messages"            => "messages#create"
+  patch  "/messages/:id/toggle" => "messages#toggle", as: "toggle_message"
+  delete "/messages/:id"        => "messages#destroy"
+
+  get   "/users/new"               => "users#new",               as: "new_user"
+  get   "/users/:id/edit"          => "users#edit",              as: "edit_user"
+  get   "/users/forgot_password"   => "users#forgot_password",   as: "forgot_password_users"
+  post  "/users"                   => "users#create",            as: "users"
+  post  "/users/send_access_email" => "users#send_access_email", as: "send_access_email_users"
+  patch "/users/:id"               => "users#update",            as: "user"
+
+  get    "/sessions/new" => "sessions#new",    as: "new_session"
+  post   "/sessions"     => "sessions#create", as: "sessions"
+  delete "/sessions/"    => "sessions#destroy"
+
+  get    "/subscriptions/new" => "subscriptions#new",    as: "new_subscription"
+  post   "/subscriptions"     => "subscriptions#create", as: "subscriptions"
+  patch  "/subscriptions"     => "subscriptions#update"
+  delete "/subscriptions"     => "subscriptions#destroy"
 
   post "/webhooks/stripe" => "webhooks#stripe"
-
-  get "/topics/:slug", to: "topics#show", as: "canonical_topic"
 
   post "/markdown/preview", to: "markdown#preview"
 
