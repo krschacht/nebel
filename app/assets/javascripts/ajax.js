7271d828eb590c22a3798c8653331bd372e18c2e
Avand <avand@avandamiri.com>
2014-05-06 13:33:59 -0700

    Closes #72: Loader

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,12 +1,29 @@
 var AJAX = {
+  showLoader: function() {
+    var loader = document.querySelector("#loader");
+    loader.classList.remove("hidden");
+  },
+
+  hideLoader: function() {
+    var loader = document.querySelector("#loader");
+    loader.classList.add("hidden");
+  },
+
   submitForm: function(form, done) {
     var xhr = new XMLHttpRequest();
 
     xhr.addEventListener("readystatechange", function() {
-      if (xhr.readyState == 4) done(xhr);
-    });
+      if (xhr.readyState == 4) {
+        this.hideLoader();
+        done(xhr);
+      }
+    }.bind(this));
 
     xhr.open(form.getAttribute("method"), form.getAttribute("action"));
     xhr.send(new FormData(form));
+    this.showLoader();
   }
 }
+
+document.addEventListener("page:before-change", AJAX.showLoader.bind(AJAX));
+document.addEventListener("page:change", AJAX.hideLoader.bind(AJAX));
